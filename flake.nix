e2ebb09ded5ce7253123fa2a1aabcab39a87979f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-07-04 08:41:07 +1000

    flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    devShells.x86_64-linux.default =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+      pkgs.mkShell {
+        buildInputs = [ pkgs.nodePackages.typescript-language-server ];
+      };
+  };
+}
