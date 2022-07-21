bb3137db36abd0814b6686821dbdf57593ea32f6
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-07-21 13:03:21 +1000

    flake compat

--- streaming-demo-app/flake.nix
+++ streaming-demo-app/flake.nix
@@ -1,7 +1,11 @@
 {
   description = "A very basic flake";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-compat }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
